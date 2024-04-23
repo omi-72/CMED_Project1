8e4c84cc7294271d147e6e8d1c2490c687fb3d45
omi-72 <omi@techetronventures.com>
2024-04-23 14:26:09 +0600

    Inside MainViewModel.kt, implement methods to start and monitor the download progress, and also DownloadManager to handle the download proces

--- app/src/main/java/com/example/cmed_project1/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/cmed_project1/viewmodel/MainViewModel.kt
@@ -1,6 +1,22 @@
 package com.example.cmed_project1.viewmodel
 
+import android.app.DownloadManager
+import android.content.Context
+import android.net.Uri
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel() {
+class MainViewModel(private val context: Context) : ViewModel() {
+
+    private lateinit var downloadManager: DownloadManager
+    private var downloadId: Long = -1
+
+    fun startDownload(url: String) {
+        downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val request = DownloadManager.Request(Uri.parse(url))
+            .setTitle("Downloading File")
+            .setDescription("Downloading")
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE)
+        downloadId = downloadManager.enqueue(request)
+    }
+
 }
